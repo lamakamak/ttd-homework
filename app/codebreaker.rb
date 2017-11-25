b1572e85a3c3c8edf6d33e7420431cb858710e59
ward <vakpko@gmail.com>
2017-11-25 23:07:59 +0100

    tested A

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -13,11 +13,40 @@ class Codebreaker
       output.puts "Enter guess:"
     end
 
+def exact_match
+
+end
+
+
     def guess(input)
       # Make sure to replace next line with the actual implemented marking algorithm,
+      @input = input
+      # if input == @secret_number
       # using the @secret_number
+      # output.puts "you typed '#{input}'"
 
-      output.puts "you typed '#{input}'"
-    end
+      if input.length != 4
+        output.puts "Try guessing a number with four digits"
+     # DONT forget outputs
+     elsif input = ''
+       output.puts ''
+
+end
   end
+
+  # def answer(input)
+  #
+  # end
+
+    def matches
+   input = []
+   input.each_with_index do |number, index|
+  if number != @secret_number
+    input << "-"
+
+    end
+end
+  end
+
+end #class
 end
